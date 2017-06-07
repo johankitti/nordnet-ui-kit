0aa2770fa9faefe80c626c0fe01c3317d9375a4c
Andreas Broström <andreas.brostrom@nordnet.se>
2017-06-07 13:19:00 +0200

    feat(Icon): Add commodity icons

--- src/components/icon/icons/index.js
+++ src/components/icon/icons/index.js
@@ -1,3 +1,4 @@
+import Aluminum from './aluminum';
 import Apartment from './apartment';
 import ArrowDown from './arrowDown';
 import ArrowLeft from './arrowLeft';
@@ -17,6 +18,7 @@ import CircleSlash from './circleSlash';
 import Clock from './clock';
 import Close from './close';
 import Cloud from './cloud';
+import Copper from './copper';
 import Edit from './edit';
 import Ellipsis from './ellipsis';
 import ExclamationPoint from './exclamationPoint';
@@ -26,24 +28,31 @@ import Film from './film';
 import Filter from './filter';
 import FloppyDisk from './floppyDisk';
 import Folder from './folder';
+import Gold from './gold';
 import GraphArea from './graphArea';
 import GraphCandlestick from './graphCandlestick';
 import GraphLine from './graphLine';
 import GraphVolume from './graphVolume';
 import Heart from './heart';
 import Key from './key';
+import Lead from './lead';
 import LightningBolt from './lightningBolt';
 import Lock from './lock';
 import Mail from './mail';
 import MailOpen from './mailOpen';
 import News from './news';
+import Nickel from './nickel';
+import Oil from './oil';
 import OrderDepth from './orderDepth';
+import Palladium from './palladium';
+import Platinum from './platinum';
 import Print from './print';
 import Profile from './profile';
 import Questionmark from './questionmark';
 import SearchIcon from './search';
 import Share from './share';
 import SignOut from './signOut';
+import Silver from './silver';
 import SocialFacebook from './socialFacebook';
 import SocialGplus from './socialGplus';
 import SocialInstagram from './socialInstagram';
@@ -51,10 +60,12 @@ import SocialTwitter from './socialTwitter';
 import Star from './star';
 import Tag from './tag';
 import TickingClock from './tickingClock';
+import Tin from './tin';
 import Trash from './trash';
 import VerticalEllipsis from './verticalEllipsis';
 
 const Icon = {
+  Aluminum,
   Apartment,
   ArrowDown,
   ArrowLeft,
@@ -74,6 +85,7 @@ const Icon = {
   Clock,
   Close,
   Cloud,
+  Copper,
   Edit,
   Ellipsis,
   ExclamationPoint,
@@ -83,24 +95,31 @@ const Icon = {
   Filter,
   FloppyDisk,
   Folder,
+  Gold,
   GraphArea,
   GraphCandlestick,
   GraphLine,
   GraphVolume,
   Heart,
   Key,
+  Lead,
   LightningBolt,
   Lock,
   Mail,
   MailOpen,
   News,
+  Nickel,
+  Oil,
   OrderDepth,
+  Palladium,
+  Platinum,
   Print,
   Profile,
   Questionmark,
   Search: SearchIcon,
   Share,
   SignOut,
+  Silver,
   SocialFacebook,
   SocialGplus,
   SocialInstagram,
@@ -108,6 +127,7 @@ const Icon = {
   Star,
   Tag,
   TickingClock,
+  Tin,
   Trash,
   VerticalEllipsis,
 };
